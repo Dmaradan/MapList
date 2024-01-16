4d641c3e5dab4f416efad82090c5e5cb832bdd8d
Dmaradan <diegomartin1313@gmail.com>
2024-01-16 16:20:12 +0100

    Enable location editing

--- MapList/ContentView.swift
+++ MapList/ContentView.swift
@@ -14,7 +14,8 @@ struct ContentView: View {
         span: MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)))
     
     @State private var locations = [Location]()
-
+    @State private var selectedPlace: Location?
+    
     var body: some View {
         MapReader { proxy in
             Map(initialPosition: startPosition) {
@@ -26,15 +27,24 @@ struct ContentView: View {
                             .frame(width: 44, height: 44)
                             .background(.white)
                             .clipShape(.circle)
+                            .onLongPressGesture {
+                                selectedPlace = location
+                            }
                     }
                 }
             }
-                .onTapGesture { position in
-                    if let coordinate = proxy.convert(position, from: .local) {
-                        let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
-                        locations.append(newLocation)
+            .onTapGesture { position in
+                if let coordinate = proxy.convert(position, from: .local) {
+                    let newLocation = Location(id: UUID(), name: "New Location", description: "", latitude: coordinate.latitude, longitude: coordinate.longitude)
+                    locations.append(newLocation)
+                }
+            }
+            .sheet(item: $selectedPlace) { place in
+                EditView(location: place) { newLocation in
+                    if let index = locations.firstIndex(of: place) {
+                        locations[index] = newLocation
                     }
-                    
+                }
             }
         }
     }
