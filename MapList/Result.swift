f878b7db7b0defd9431ec7560fb17374d73c7236
Dmaradan <diegomartin1313@gmail.com>
2024-01-17 15:44:02 +0100

    Add fetch nearyby locations functionality

---
+++ MapList/Result.swift
@@ -0,0 +1,22 @@
+//
+//  Result.swift
+//  MapList
+//
+//  Created by Diego Martin on 1/17/24.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let query: Query
+}
+
+struct Query: Codable {
+    let pages: [Int: Page]
+}
+
+struct Page: Codable {
+    let pageid: Int
+    let title: String
+    let terms: [String: [String]]?
+}
